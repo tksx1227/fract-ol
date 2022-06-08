cfd4f3ae64d9144fc21c8fa01cef4fbf24faddc2
tomori <tmrtkm1227tksx@gmail.com>
2022-06-08 22:27:45 +0900

    :sparkles: 構造体を追加
    - t_data, t_vars

--- include/fractol.h
+++ include/fractol.h
@@ -1,15 +1,23 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# include <stdlib.h>
+# include <stdio.h>
 # include <mlx.h>
 
-typedef struct s_data
+typedef struct	s_data
 {
-    void *img;
-    char *addr;
-    int bits_per_pixel;
-    int line_length;
-    int endian;
-} t_data;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+typedef struct	s_vars
+{
+	void	*mlx;
+	void	*win;
+}	t_vars;
 
 #endif
