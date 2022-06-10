8b35a335a06a95438ffa59c8201e33e046a6a2d4
tomori <tmrtkm1227tksx@gmail.com>
2022-06-11 00:41:02 +0900

    :recycle: リファクタリング
    - フラクタル関連の関数をfractal_utils.cで定義する

--- src/fractal_utils.c
+++ src/fractal_utils.c
@@ -1,29 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   fractal_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/09 12:03:39 by ttomori           #+#    #+#             */
-/*   Updated: 2022/06/10 09:47:37 by ttomori          ###   ########.fr       */
+/*   Created: 2022/06/11 00:25:59 by ttomori           #+#    #+#             */
+/*   Updated: 2022/06/11 00:25:59 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-t_comp	init_comp(double re, double im)
+bool	is_mandelbrot(t_comp comp)
 {
-	t_comp	comp;
+	int		i;
+	t_comp	z;
 
-	comp.re = re;
-	comp.im = im;
-	return (comp);
-}
-
-
-int	create_trgb(unsigned char t, unsigned char r,
-				unsigned char g, unsigned char b)
-{
-	return (*(int *)(unsigned char [4]){b, g, r, t});
+	z = init_comp(0.0, 0.0);
+	i = 0;
+	while (i < 30)
+	{
+		if (comp_dist(z) > 2.0)
+			return (false);
+		z = comp_add(comp_mul(z, z), comp);
+		i++;
+	}
+	return (true);
 }
